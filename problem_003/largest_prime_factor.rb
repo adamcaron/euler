1b037a888a51440abcfe1caf1d022cd3973a7477
Adam Caron <adamwcaron@gmail.com>
2015-10-28 17:23:41 -0600

    Add problem 3

---
+++ problem_003/largest_prime_factor.rb
@@ -0,0 +1,26 @@
+# find the prime factors of a number
+
+# can it be divided by a number
+# other than 1 or itself?
+
+# Does it have any divisors other than itself and 1?
+
+# how can we check if a number
+# has any divisors other than 1 and itself?
+
+# given 6
+# is it divisible by 2?
+
+# If a number ends in 2, 4, 6, 8, or 0,
+# then one of its factors is 2
+
+# If a number ends in 5 or 0,
+# then one of its factors is 5
+
+# Now that we've found all the multiples
+# of two through ten,
+# we've found all the prime numbers
+# because they only have factors of 1 & themselves.
+
+# Primes up to 100 are:
+# []
---
+++ problem_003/largest_prime_factor_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './largest_prime_factor'
+require 'pry'
+
+class LargestPrimeFactorTest < Minitest::Test
+
+end
