2dd6b14905753fd398a6c66d0153cb0a0b688c62
Adam Caron <adamwcaron@gmail.com>
2016-02-11 16:01:02 -0700

    Next steps

--- problem_003_largest_prime_factor/largest_prime_factor.rb
+++ problem_003_largest_prime_factor/largest_prime_factor.rb
@@ -9,6 +9,11 @@ class LargestPrimeFactorFinder
   # self.factors or self.prime?
   # RubyProf: https://github.com/ruby-prof/ruby-prof
 
+  # http://stackoverflow.com/questions/3398159/all-factors-of-a-given-number
+
+  # Is it possible and quicker to get all the primes of a number,
+  # then, of those primes, determine which is the highest factor?
+
   def self.factors(number)
     some_factors = []
     1.upto(number) do |n|
@@ -31,6 +36,14 @@ class LargestPrimeFactorFinder
       # means by which we check if it's prime.
       # in the seive of eratosthenes,
       # only numbers between 2 and 9 need to be checked [is this true? ... if so, let's do this and do it concurrently]
+
+      # 2, 3, 5, 7, 11
+      # only up to the square root of the number
+
+      # step counting http://www.dotnetperls.com/iterator-ruby
+      # use step counting to step over those numbers that aren't applicable.
+      # take an array of numbers and nullify anything that's not prime. (Sieve of Eratosthenes)
+
       return false if number % n == 0
     end
     true
